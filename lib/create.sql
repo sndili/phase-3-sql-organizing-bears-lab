47f26dc98d0628c2e2c0d6bac179cf90ceb3ac7a
Sylvia <ndilisylvia@gmail.com>
2022-10-25 15:38:47 +0300

    done

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+ID INTEGER PRIMARY KEY,
+Name TEXT,
+Age INTEGER,
+Sex TEXT,
+Color TEXT,
+Temperament TEXT,
+Alive TEXT
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 30, 'M', 'black', 'yes', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 31, 'F', 'black', 'yes', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 32, 'M', 'black', 'yes', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 33, 'F', 'black', 'yes', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 34, 'M', 'black', 'yes', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 35, 'F', 'black', 'yes', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 36, 'M', 'black', 'yes', 'True');
+INSERT INTO bears (name) VALUES (NULL);
